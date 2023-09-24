8b1ae7d7e4cf6aa9b71533e4edd07c2eb1ff5e51
chunchunz i <ichunchunz@163.com>
2023-09-24 10:33:45 +0800

    更新网络读取data.json

--- SmallTool/MainWindow.xaml.cs
+++ SmallTool/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using Microsoft.Win32;
 using System.Windows.Input;
+using System.Net;
 
 namespace SmallTool
 {
@@ -18,7 +19,25 @@ namespace SmallTool
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> actoffice_commands = new List<string>
+        {
+            "cscript //Nologo \"C:\\Program Files\\Microsoft Office\\Office16\\ospp.vbs\" /sethst:kms.03k.org",
 
+            "cscript //Nologo \"C:\\Program Files\\Microsoft Office\\Office16\\ospp.vbs\" /inpkey:FXYTK-NJJ8C-GB6DW-3DYQT-6F7TH",
+            // "cscript //Nologo \"C:\\Program Files\\Microsoft Office\\Office16\\ospp.vbs\" /inpkey:KNH8D-FGHT4-T8RK3-CTDYJ-K2HT4",
+            // "cscript //Nologo \"C:\\Program Files\\Microsoft Office\\Office16\\ospp.vbs\" /inpkey:FTNWT-C6WBT-8HMGF-K9PRX-QV9H8",
+            "cscript //Nologo \"C:\\Program Files\\Microsoft Office\\Office16\\ospp.vbs\" /act",
+
+        };
+
+        List<string> commands = new List<string>
+                {
+                    "cscript //Nologo %windir%\\system32\\slmgr.vbs /upk",
+                    "cscript //Nologo %windir%\\system32\\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX",
+                    "cscript //Nologo %windir%\\system32\\slmgr.vbs /skms kms.03k.org",
+                    "cscript //Nologo %windir%\\system32\\slmgr.vbs /ato"
+                };
+        string result = string.Empty;
         public class Node
         {
             public string Name { get; set; }
@@ -31,6 +50,7 @@ namespace SmallTool
             PCName.Text = Environment.MachineName;
             IP.Text = GetIpV4();
             NetCard.Text = GetNetCard()[1];
+            CheckisexistAsync();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -46,11 +66,12 @@ namespace SmallTool
             {
                 List<string> netcardlist = GetNetCard();
                 SaveToJson(netcardlist[0], PCName.Text, IP.Text);
+                MessageBox.Show($"{netcardlist[0]}已保存");
             }
 
         }
 
-        private void CmdRun(string command)
+        private string CmdRun(string command)
         {
             Process process = new Process();
 
@@ -67,8 +88,10 @@ namespace SmallTool
             process.Start();
             process.StandardInput.WriteLine(command);
             process.StandardInput.WriteLine("exit");
+            string output = process.StandardOutput.ReadToEnd();
             process.WaitForExit();
             process.Close();
+            return output;
         }
 
 
@@ -172,7 +195,7 @@ namespace SmallTool
                         //InterNetwork    IPV4地址      InterNetworkV6        IPV6地址
                         //Max            MAX 位址
                         if (ipadd.Address.AddressFamily == AddressFamily.InterNetwork)
-                        {                         
+                        {
                             return ipadd.Address.ToString();
                         }
                         //判断是否为ipv4                  
@@ -183,7 +206,7 @@ namespace SmallTool
                 {
                     return "";
                 }
-                
+
             }
             return "";
         }
@@ -196,6 +219,153 @@ namespace SmallTool
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            megbox secwin = new megbox();
+            secwin.Owner = this;
+            secwin.megboxlbl.Content = "windows激活中...";
+            secwin.Show();
+
+            foreach (var command in commands)
+            {
+                result = CmdRun(command);
+            }
+            if (result.Contains("成功地激活"))
+            {
+                secwin.megboxlbl.Content = "激活成功";
+                // MessageBox.Show("激活成功");
+            }
+            else
+            {
+                secwin.megboxlbl.Content = "激活成失败";
+                // MessageBox.Show("激活失败");
+            }
+        }
+
+        private void ButtonBase_OnClick1(object sender, RoutedEventArgs e)
+        {
+            megbox secwin = new megbox();
+            secwin.Owner = this;
+            secwin.megboxlbl.Content = "office激活中...";
+            secwin.Show();
+
+            foreach (var command in actoffice_commands)
+            {
+                result = CmdRun(command);
+            }
+            if (result.Contains("Product activation successful"))
+            {
+                secwin.megboxlbl.Content = "激活成功";
+                // MessageBox.Show("激活成功");
+            }
+            else
+            {
+                secwin.megboxlbl.Content = "激活成失败";
+                // MessageBox.Show("激活失败");
+            }
+        }
+
+        private void CheckisexistAsync()
+        {
+            string content = "";
+            string networkFileUrl = "http://10.0.1.18/DATA.JSON"; // 替换为网络文件的URL
+            string localFilePath = "Data.json"; // 本地文件的路径
+
+            try
+            {
+                WebClient client = new WebClient();
+                content = client.DownloadString(networkFileUrl);
+            }
+            catch (WebException)
+            {
+                // 如果网络读取失败，尝试从本地读取文件
+                try
+                {
+                    content = File.ReadAllText(localFilePath);
+
+                    // 如果成功从本地读取文件，将内容写入TextBox
+                }
+                catch (Exception ex)
+                {
+                    // 处理本地文件读取失败的异常
+                    MessageBox.Show("无法读取文件：" + ex.Message);
+                }
+            }
+
+
+            JObject jsonstring = JObject.Parse(content);
+
+            Dictionary<string, Node> node =
+                JsonConvert.DeserializeObject<Dictionary<string, Node>>(jsonstring.ToString());
+
+            string netcardmac = GetNetCard()[0];
+
+            foreach (var mac in node)
+            {
+                if (mac.Key == netcardmac)
+                {
+                    autochangebtn.Visibility = Visibility.Visible;
+                    break;
+                }
+            }
+        }
+
+
+
+        private void autochangebtn_Click(object sender, RoutedEventArgs e)
+        {
+            //read json
+            string filePath = "Data.json";
+
+            string content = File.ReadAllText(filePath);
+            JObject jsonstring = JObject.Parse(content);
+
+            Dictionary<string, Node> node =
+                JsonConvert.DeserializeObject<Dictionary<string, Node>>(jsonstring.ToString());
+
+            var machineMac = GetNetCard()[0];
+
+            foreach (var mac in node)
+            {
+                if (mac.Key == machineMac)
+                {
+                    var ip = mac.Value.Ip;
+                    var pcName = mac.Value.Name;
+
+                    if (ip != GetIpV4() || Environment.MachineName != pcName)
+                    {
+                        string host = string.Join(".", ip.Split('.')[0], ip.Split('.')[1], ip.Split('.')[2], "1");
+                        string gateway = "255.255.255.0";
+                        string dns1 = "114.114.114.114";
+                        string dns2 = "8.8.8.8";
+                        string netcard = GetNetCard()[1];
+
+                        string command = $"netsh interface ip set address \"{netcard}\" static {ip} {gateway} {host}";
+                        CmdRun(command);
+                        command = $"netsh interface ip set dns \"{netcard}\" static {dns1} primary";
+                        CmdRun(command);
+                        command = $"netsh interface ip add dns \"{netcard}\" {dns2} index=2";
+                        CmdRun(command);
+
+                        #region 更改电脑名称
+                        RegistryKey pregkey;
+                        pregkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\ControlSet001\\Control\\ComputerName\\ComputerName", true);
+                        pregkey.SetValue("ComputerName", pcName);
+                        pregkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\ControlSet001\\Services\\Tcpip\\Parameters", true);
+                        pregkey.SetValue("NV Hostname", pcName);
+                        pregkey.SetValue("Hostname", pcName);
+                        pregkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\ComputerName\\ComputerName", true);
+                        pregkey.SetValue("ComputerName", pcName);
+                        pregkey = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\Tcpip\\Parameters", true);
+                        pregkey.SetValue("NV Hostname", pcName);
+                        #endregion
+                        MessageBox.Show("已更改");
+
+                    }
+                }
+            }
         }
     }
 }
---
+++ SmallTool/megbox.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SmallTool
+{
+    /// <summary>
+    /// megbox.xaml 的交互逻辑
+    /// </summary>
+    public partial class megbox : Window
+    {
+        public megbox()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
